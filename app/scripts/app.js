8c9118102eb61a89969548a3dcb4139341effe4b
unknown <alumno@LAB02PC15.labs.extension>
2016-09-07 19:44:56 -0300

    cambio en app y form

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,5 +3,14 @@ angular
   .controller('MiCtrl', function($scope){
     $scope.mail = "mi@mails";
     $scope.vista = 1;
+    //$scope.cambiarVista = cambiarVista; //si lo declaro asi le da la funcion entera, si le pongo parentesis seria el return
+    $scope.form={};
+    $scope.enviar=function(){
+    	console.log($scope.form);
+    }
+
+    $scope.cambiarVista = function(vista){
+    	$scope.vista=vista;
+    }
     var unmail = "dsd";
   })
