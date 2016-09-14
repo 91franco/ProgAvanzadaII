4a8ac7fcb7178624cf1afbb12a677941809ac6f8
unknown <alumno@LAB02PC15.labs.extension>
2016-09-14 20:39:03 -0300

    creacion de controler y servicio

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,38 +1,7 @@
 angular
   .module('miApp', ['ngAnimate'])
-  .controller('MiCtrl', function($scope,$http){
-    $scope.mail = "mi@mails";
-    $scope.vista = 1;
-    $scope.enviado=true;
-    $scope.aPartir=1;
-    
-    $scope.form={};
-    $scope.enviar=function(){
-    	console.log($scope.form);
-    	 $scope.enviado=false;
-    }
 
-    //$scope.cambiarVista = cambiarVista; //si lo declaro asi le da la funcion entera, si le pongo parentesis seria el return
-    $scope.cambiarVista = function(vista){
-    	$scope.vista=vista;
-    }
-    var unmail = "dsd";
-    $scope.personas = [];
-
-
-    $http
-    	.get('./scripts/persona.json')
-    	.then(function(rta){
-    		console.log(rta)
-    		 $scope.personas = rta.data;
-    	})
-    	.catch(function(error){
-    		console.log(error)
-    	})
-
-    	console.log('Despues del get');
-})
-.animation('.slide', [function() {
+  .animation('.slide', [function() {
   return {
     // make note that other events (like addClass/removeClass)
     // have different function input parameters
---
+++ app/scripts/servicios/data.js
@@ -0,0 +1,17 @@
+angular
+	.module('miApp')
+	.factory('Data',[function(){
+		var data = [];
+		return {
+			listado:listado,
+			cargar:cargar
+		}
+
+		function cargar(objeto){
+			data.push(objeto);
+		}
+
+		function listado(){
+			return data;
+		}
+	}]);
